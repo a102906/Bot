63fed6169f18e34898076ff4c2906ad2957a8df7
a102906 <99275952+a102906@users.noreply.github.com>
2022-03-01 21:27:23 +0500

    Update examwork.py

--- examwork.py
+++ examwork.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.INFO)
 # Initialize bot and dispatcher
 dp = Dispatcher(bot)
 
-welcome_message=("Добро пожаловать!\nЭто бот рекламного агентства Lemon Media!\n"
+welcome_message=("Добро пожаловать!\nЭто бот рекламного агентства \n"
                  "Он поможет вам ознакомиться с нашими ценами и нашей комапиней.")
 
 @dp.message_handler(commands=['start', 'help'])
@@ -32,17 +32,12 @@ async def infocat(message: types.Message):
 async def backtomainmenu(message: types.Message):
     await message.answer('Какой раздел выберите?', reply_markup=mainmenu)
 
-text=('Торговая марка Lemon Media предлагает свои услуги по размещению рекламы в сфере InDoor - рекламы.\n'
-      'Наша компания начиная с 2018 года оказывала свои услуги таким крупным компаниям как “PEPSI”, '
-      '“BLISS”, “Uztelecom” и другим компаниям которые ведут свою деятельность в нашей республике. ')
+text=('')
 @dp.message_handler(text = 'О компании')
 async def company_info(message: types.Message):
     await message.answer(text)
 
-mon_info=('''Наша компания сотрудничает с такими сетями кафе, такими как 
-"Evos", "Les Ailes", "Totus" и другими заведениями  по всей республике.
-Мы размещаем рекламу на мониторах которые находятся в залах этих заведений. \n
-Количество филиалов  по Республике - 56.\n Ежемесячная аудитория - более чем 1,5 млн.  человек. ''')
+mon_info=('''''')
 @dp.message_handler(text = 'Нас смотрят')
 async def monit_info(message: types.Message):
     await message.answer(mon_info)
@@ -50,21 +45,21 @@ async def monit_info(message: types.Message):
 @dp.message_handler(text = '60 показов  в день')
 async def sixtyt(message: types.Message):
     await message.answer(''' 60 показов в день.\nЧастота показов в рекламных  блоках -в каждом рекламном блоке по 2 раза.\n
-Цена на размещение  при месячном пакете  (без скидки) - 19 000 000.\nПри долгосрочном договоре имеются скидки.''')
+Цена на размещение  при месячном пакете  (без скидки) - 19 000 .\nПри долгосрочном договоре имеются скидки.''')
 
 @dp.message_handler(text = '45 показов  в день')
 async def f_ft(message: types.Message):
     await message.answer(''' 45 показов в день.\nЧастота показов в рекламных  блоках -в каждом рекламном блоке.\n
-Цена на размещение  при месячном пакете  (без скидки) - 15 000 000.\nПри долгосрочном договоре имеются скидки.''')
+Цена на размещение  при месячном пакете  (без скидки) - 15 000 .\nПри долгосрочном договоре имеются скидки.''')
 
 @dp.message_handler(text = '30 показов  в день')
 async def thirtyt(message: types.Message):
     await message.answer(''' 30 показов в день.\nЧастота показов в рекламных  блоках -в каждом 2м рекламном блоке.\n
-Цена на размещение  при месячном пакете  (без скидки) - 11 000 000.\nПри долгосрочном договоре имеются скидки.''')
+Цена на размещение  при месячном пакете  (без скидки) - 11 000 .\nПри долгосрочном договоре имеются скидки.''')
 
 @dp.message_handler(text = '☎️Обратная связь')
 async def monit_info(message: types.Message):
-    await message.answer(''' По поводу сотрудничества или если у вас возникли вопросы звоните по номеру: +998 99 828-88-28. 
+    await message.answer(''' По поводу сотрудничества или если у вас возникли вопросы звоните по номеру:
 Так же можете написать на этот же номер по Telegram.''')
 
 if __name__ == '__main__':
